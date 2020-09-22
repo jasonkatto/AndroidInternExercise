679b84ddf697d3c1ff345db59c4404125e93cb6e
jason katto <jasonkatto@gmail.com>
2020-09-22 12:37:08 -0400

    Initial commit

---
+++ app/src/main/java/com/kayak/internexercise/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.kayak.internexercise
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // TODO implement airline list
+    }
+}
